4323e65f94ec9f3099a5d84aa4fcda019c6c9a72
ogparry1 <ogparry1@ufl.edu>
2017-08-28 12:51:22 -0400

    No change

--- listingController.js
+++ listingController.js
@@ -7,8 +7,16 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
       Implement these functions in the controller to make your application function 
       as described in the assignment spec. 
      */
-    $scope.addListing = function() {};
-    $scope.deleteListing = function(index) {};
-    $scope.showDetails = function(index) {};
+    $scope.addListing = function() {
+    	
+    };
+    
+    $scope.deleteListing = function(index) {
+    	
+    };
+    
+    $scope.showDetails = function(index) {
+    	
+    };
   }
 ]);
