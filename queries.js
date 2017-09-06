5fe3b56b17e2b77eeb6fefe00ef64debd78c9041
ogparry1 <ogparry1@ufl.edu>
2017-09-06 02:28:08 -0400

    Assignment 3 Essentially Finished

--- queries.js
+++ queries.js
@@ -1,3 +1,8 @@
+var mongoose = require('mongoose'),
+    config = require('./config'),
+    Listing = require('./ListingSchema');
+
+mongoose.connect(config.db.uri);
 /* Fill out these functions using Mongoose queries*/
 
 var findLibraryWest = function() {
@@ -5,6 +10,11 @@ var findLibraryWest = function() {
     Find the document that contains data corresponding to Library West,
     then log it to the console. 
    */
+	var item = Listing.find({ name: "Library West" },
+	function(err, data) {
+		if (err) throw err;
+		console.log('Library West Found:\n' + data);
+	});
 };
 var removeCable = function() {
   /*
@@ -12,20 +22,36 @@ var removeCable = function() {
     on cable TV. Since we live in the 21st century and most courses are now web based, go ahead
     and remove this listing from your database and log the document to the console. 
    */
+	var item = Listing.findOneAndRemove({ code: "CABL" },
+	function(err) {
+		if (err) throw err;
+		console.log('CABL removed');
+	});
 };
 var updatePhelpsMemorial = function() {
   /*
     Phelps Memorial Hospital Center's address is incorrect. Find the listing, update it, and then 
     log the updated document to the console. 
    */
+	var item = Listing.findOneAndUpdate({ name: "Phelps Memorial Hospital Center" },
+	{ address: "701 N Broadway, Sleepy Hollow, NY 10591" },
+	function(err, data) {
+		if (err) throw err;
+		console.log('Phelps Hospital Updated\n' + data);
+	});
 };
 var retrieveAllListings = function() {
   /* 
     Retrieve all listings in the database, and log them to the console. 
    */
+	var item = Listing.find({},
+	function(err, data) {
+		if (err) throw err;
+		console.log('All Listings:\n' + data);
+	});
 };
 
-findLibraryWest();
-removeCable();
-updatePhelpsMemorial();
+//findLibraryWest();
+//removeCable();
+//updatePhelpsMemorial();
 retrieveAllListings();
