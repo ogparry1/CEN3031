5ebf870c2fea6a34b7da4db416e45fd4bf883b9c
ogparry1 <ogparry1@ufl.edu>
2017-09-19 20:32:53 -0400

    Mocha finally working

--- server/controllers/listings.server.controller.js
+++ server/controllers/listings.server.controller.js
@@ -7,6 +7,7 @@ var mongoose = require('mongoose'),
   In this file, you should use Mongoose queries in order to retrieve/add/remove/update listings.
   On an error you should send a 404 status code, as well as the error message. 
   On success (aka no error), you should send the listing(s) as JSON in the response.
+
   HINT: if you are struggling with implementing these functions, refer back to this tutorial 
   from assignment 3 https://scotch.io/tutorials/using-mongoosejs-in-node-js-and-mongodb-applications
  */
@@ -98,6 +99,7 @@ exports.list = function(req, res) {
 
 /* 
   Middleware: find a listing by its ID, then pass it to the next request handler. 
+
   HINT: Find the listing using a mongoose query, 
         bind it to the request object as the property 'listing', 
         then finally call next
